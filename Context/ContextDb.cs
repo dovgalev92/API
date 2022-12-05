99cf5911793cd94eaccfcc30d9ecacc7d6ae0ffc
dovgalev92 <dovgalev2021@inbox.ru>
2022-12-06 00:40:07 +0300

    Добавил класс контекста данных, подлючение к базе данных

---
+++ Context/ContextDb.cs
@@ -0,0 +1,13 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+namespace API.Context
+{
+    public class ContextDb : DbContext
+    {
+        public ContextDb(DbContextOptions<ContextDb> options) : base(options) { }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Warehouse> Warehouses { get; set; }
+        public DbSet<WarehouseRoom> WarehouseRooms { get; set; }
+        public DbSet<Region> Regions { get; set; }
+    }
+}
