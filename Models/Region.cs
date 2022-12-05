b05f6f1db78b58f2894fd3f61fc005a5ae398478
dovgalev92 <dovgalev2021@inbox.ru>
2022-12-06 00:31:07 +0300

    Добавьте файлы проекта.

---
+++ Models/Region.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models
+{
+    public class Region
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int? WarehouseId { get; set; }
+        [NotMapped]
+        public List<Warehouse>? Warehouse { get; set; }
+    }
+}
---
+++ Models/Warehouse.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+
+namespace API.Models
+{
+    public class Warehouse
+    {
+        public int Id { get; set; }
+        [DisplayName("Название склада")]
+        public string? Name { get; set; }
+        [DisplayName("Собственник склада")]
+        public int? CompanyId { get; set; }
+        [DisplayName("Место расположения")]
+        public int? RegionId { get; set; }
+        [NotMapped]
+        public Company? Company { get; set; }
+        [NotMapped]
+        public Region? Region { get; set; }
+        public List<WarehouseRoom>? WarehouseRooms { get; set; }
+        /// <summary>
+        /// количество комнат
+        /// </summary>
+        [NotMapped]
+        public int roomCount { get; set; }
+        /// <summary>
+        /// Наименование комнат
+        /// </summary>
+        [NotMapped]
+        public string? name_compartment { get; set; }
+    }
+}
